5cfa5e48b419f16ec6c6c6d8fa071cdcdc003f37
waris0129 <waris0129@hotmail.com>
2020-10-17 10:41:17 -0500

    Second commit: Add two more modules Loosely Coupling Interface implementation

---
+++ Spring-00-LooselyCoupled/src/service/FullTimeMentor.java
@@ -0,0 +1,12 @@
+package service;
+
+import implementation.Mentor;
+
+public class FullTimeMentor implements Mentor {
+
+
+    @Override
+    public void createAccount() {
+        System.out.println("Full Time Mentor is created....");
+    }
+}
--- Spring-00-TightlyCoupled/src/CybertekApp.java
+++ Spring-00-TightlyCoupled/src/CybertekApp.java
@@ -10,6 +10,8 @@ public class CybertekApp {
         mentor.manageAccount();
 
 
+
+
     }
 
 
