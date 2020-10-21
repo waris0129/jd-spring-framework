21e1127d24bf242187d98f5817f0f797b8f8244c
waris0129 <waris0129@hotmail.com>
2020-10-20 19:28:18 -0500

    9th commit: new module Setter Injection

--- Spring-02-DI-ConstructorInjection/src/main/java/com/cybertek/servcies/Java.java
+++ Spring-02-DI-ConstructorInjection/src/main/java/com/cybertek/servcies/Java.java
@@ -1,15 +1,20 @@
 package com.cybertek.servcies;
 
 import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSessions;
 
 public class Java implements Course {
 
-    OfficeHours officeHours;
+    ExtraSessions extraSessions; // this is a denpendency class, it must inject object
 
+    public Java(ExtraSessions extraSessions) { // Important , we implement Interface and abstract method
+        this.extraSessions= extraSessions;
+    }
+    // java class dependencies for OfficeHours class
 
 
     @Override
     public void getTeachingHours() {
-        System.out.println("Weekly Teaching Hours : "+ 20 + officeHours.getHours());
+        System.out.println("Weekly Teaching Hours : "+ 20 + extraSessions.getHours());
     }
 }
---
+++ Spring-02-DI-SetterInjection/src/main/java/com/cybertek/services/Java.java
@@ -0,0 +1,22 @@
+package com.cybertek.services;
+
+import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSessions;
+
+public class Java implements Course {
+
+    private OfficeHours officeHours;
+
+    public OfficeHours getOfficeHours() {
+        return officeHours;
+    }
+
+    public void setOfficeHours(OfficeHours officeHours){ // spring parse setOfficeHours as set + officeHours
+        this.officeHours = officeHours;
+    }
+
+    @Override
+    public void getTeachingHours() {
+        System.out.println("Weekly Teaching Hours : "+ 20+officeHours.getHours());
+    }
+}
