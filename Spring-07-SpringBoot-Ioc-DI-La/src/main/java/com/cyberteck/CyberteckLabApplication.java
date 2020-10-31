e4c9c6f750210f708860d7d0336c9edebb105fec
waris0129 <waris0129@hotmail.com>
2020-10-31 12:31:00 -0500

    18th commit: Day5 SpringBoot Carpet Lab

---
+++ Spring-07-SpringBoot-Ioc-DI-La/src/main/java/com/cyberteck/CyberteckLabApplication.java
@@ -0,0 +1,13 @@
+package com.cyberteck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberteckLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberteckLabApplication.class, args);
+	}
+
+}
---
+++ Spring-07-SpringBoot-Ioc-DI-La/src/test/java/com/cyberteck/CyberteckLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyberteck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberteckLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
